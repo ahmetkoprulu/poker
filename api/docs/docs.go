8b11ef9f3ffdada965cce9741ce1c106b5d192b5
ahmet koprulu <aensarkoprulu@gmail.com>
2025-02-17 20:39:12 +0300

    [Api] Updated swagger hostname

--- api/docs/docs.go
+++ api/docs/docs.go
@@ -1042,7 +1042,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "poker-api.ahmetkoprulu.com",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "RTRP API",
